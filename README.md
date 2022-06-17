4ab4fd0596597f860b90decadde8f674265864a7
AElisee <2kange.eli@gamil.com>
2022-06-17 16:35:27 +0000

    projet final

---
+++ README.md
@@ -0,0 +1,5 @@
+service email: service_feb5u4n
+template email: template_x9qmj6s
+
+user ID / public : gp0n1h4Yi8-THrO3C
+Access Token / private : BrV6jhIEoo60ww07FLbRm
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FormTemplate } from "./FormTemplate.js";
+
+const App = () => {
+  return (
+    <div>
+      <FormTemplate />
+    </div>
+  );
+};
+
+export default App;
